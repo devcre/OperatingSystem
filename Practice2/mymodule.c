54fec98fdfb3e35edadfd9502e2671638d1d5bd2
devcre <tigertank707@gmail.com>
2019-03-22 17:08:17 +0900

    Add files via upload

---
+++ Practice2/mymodule.c
@@ -0,0 +1,20 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+int simple_init(void)
+{
+    printk(KERN_INFO "Loading My Module............\n");
+    return 0;
+    /* initialization routine */
+}
+
+void simple_exit(void)
+{
+    printk(KERN_INFO "Removing My Module............\n");
+    /* exit routine */
+}
+
+/* initialization, exit routine setting */
+module_init(simple_init);
+module_exit(simple_exit);
